7c3fa0fcc17f779b5dadda6f838d4967002dc02f
mehdi.boudart <mehdi.boudart@inetch.lu>
2020-01-24 20:59:43 +0100

    add env variable

--- .config/fish/conf.d/env-config.fish
+++ .config/fish/conf.d/env-config.fish
@@ -1,1 +1,7 @@
-set -x GRAALVM_HOME /usr/lib/jvm/graalvm/
+# Env Variable
+set -x GRAALVM_HOME /usr/lib/jvm/graalvm
+set -x JAVA_HOME (readlink -f /usr/bin/java | sed "s:bin/java::")
+set -x PATH $PATH ~/.npm-global/bin
+set -x PATH $PATH /home/boud/.minishift/cache/oc/v3.11.0/linux 
+set -x PATH $PATH /home/boud/Openshift
+set -x PATH $PATH $GRAALVM_HOME/bin
