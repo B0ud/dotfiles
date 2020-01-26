80a1875fbb3df0060a731375ee20b1cc9070b87e
mehdi.boudart <mehdi.boudart@inetch.lu>
2020-01-26 16:22:20 +0100

    Fix locale errors

--- .config/fish/conf.d/env-config.fish
+++ .config/fish/conf.d/env-config.fish
@@ -5,3 +5,8 @@ set -x PATH $PATH ~/.npm-global/bin
 set -x PATH $PATH /home/boud/.minishift/cache/oc/v3.11.0/linux 
 set -x PATH $PATH /home/boud/Openshift
 set -x PATH $PATH $GRAALVM_HOME/bin
+
+#set -x LC_ALL  fr_FR.utf8
+#set -x LANG    fr_FR.utf8
+#set -x LANGUAGE   fr_FR.utf8
+set -x LC_CTYPE fr_FR.utf8
